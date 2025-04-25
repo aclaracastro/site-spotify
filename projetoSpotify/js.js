3969ea78aa953fd04561de3ea85d370c71ec26fd
Ana Clara Castro <103144032+aclaracastro@users.noreply.github.com>
2025-04-25 10:23:22 -0400

    Update js.js

--- projetoSpotify/js.js
+++ projetoSpotify/js.js
@@ -3,34 +3,50 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(){
-    resultPlaylist.classList.add('hidden');
-    const artistName = document.getElementById('artist-name'); 
-    const artistImage = document.getElementById('artist-img');
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add("hidden");
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = ''; // Limpa os resultados anteriores    
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
+    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm) );
 
-    resultArtist.classList.remove('hidden');
 
+  filteredArtists.forEach(artist => {    
+      const artistCard = document.createElement('div');
+      artistCard.classList.add('artist-card');
+
+      artistCard.innerHTML = `
+          <div class="card-img">
+              <img class="artist-img" src="${artist.urlImg}" />
+              <div class="play">
+                  <span class="fa fa-solid fa-play"></span>
+              </div>
+          </div>
+      <div class="card-text">              
+              <span class="artist-name">${artist.name}</span>
+              <span class="artist-categorie">Artista</span>
+          </div>
+      `;
+       gridContainer.appendChild(artistCard);
+  });
+
+  resultArtist.classList.remove('hidden');
 }
-
 
 document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
+    const searchTerm = searchInput.value.toLowerCase().trim();
+
     if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+        return;
     }
-    
+
     requestApi(searchTerm);
-})
+});
